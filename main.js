630ed2a0f701d1cb4b29d9b6c76a115d4a9aa37c
julinhodoceu <julio06.sp@gmail.com>
2023-04-21 00:13:03 -0300

    att exercicio

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
-//document.querySelector('.tecla_pom').onclick = tocaSomPom;
 const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
-function tocaSomPom() {
-document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
-while (contador < listaDeTeclas.length) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
-    contador++
-    console.log (contador)
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+
+        tocaSom(idAudio)
+
+    };
+
+    tecla.onkeydown = function(evento) {
+
+        if (evento.code == 'Space') {
+        tecla.classList.add('ativa')
+     }
+    }
+    tecla.onkeyup = function() {
+        tecla.classList.remove('ativa')
+    }
 }
